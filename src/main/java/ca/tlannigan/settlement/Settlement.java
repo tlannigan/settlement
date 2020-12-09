b1681c60f0a06a0b4a00ee1575647fc7ea2cde20
tlannigan <tristanlannigan@gmail.com>
2020-12-08 19:30:15 -0700

    Add description command

--- src/main/java/ca/tlannigan/settlement/Settlement.java
+++ src/main/java/ca/tlannigan/settlement/Settlement.java
@@ -1,11 +1,13 @@
 package ca.tlannigan.settlement;
 
+import ca.tlannigan.settlement.commands.CommandDescription;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Settlement extends JavaPlugin {
     @Override
     public void onEnable(){
         getLogger().info("onEnable is called!");
+        this.getCommand("description").setExecutor(new CommandDescription());
     }
 
     @Override
--- src/main/java/ca/tlannigan/settlement/commands/CommandDescription.java
+++ src/main/java/ca/tlannigan/settlement/commands/CommandDescription.java
@@ -3,11 +3,18 @@ package ca.tlannigan.settlement.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandDescription implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        return false;
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            player.sendMessage("Reduce the monotony. Construct all your base essentials with ease, and upgrade them along the way");
+        }
+
+        return true;
     }
 }
