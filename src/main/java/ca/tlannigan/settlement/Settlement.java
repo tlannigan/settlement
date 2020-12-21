f908392ba6487c114aae076b242c518a659927be
tlannigan <tristanlannigan@gmail.com>
2020-12-20 22:35:51 -0700

    Add config file and move Mongo connection string to config

--- src/main/java/ca/tlannigan/settlement/Settlement.java
+++ src/main/java/ca/tlannigan/settlement/Settlement.java
@@ -1,23 +1,32 @@
 package ca.tlannigan.settlement;
 
 import ca.tlannigan.settlement.commands.CommandDescription;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Settlement extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Settlements will reign");
-        this.getCommand("description").setExecutor(new CommandDescription());
-        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+
+        // Create default config.yml if one is not available
+        this.saveDefaultConfig();
+        FileConfiguration config = this.getConfig();
 
         // Connect to MongoDB
-//        MongoClient mongoClient = MongoClients.create(
-//                "mongodb+srv://tlannigan:PmU9F334CtgZiuC@spigotcluster.ahu5k.mongodb.net/sample_weatherdata?retryWrites=true&w=majority");
-//        MongoDatabase database = mongoClient.getDatabase("sample_weatherdata");
-//        MongoCollection<Document> coll = database.getCollection("data");
+        String connString = config.getString("mongo_connection_string");
+        MongoClient mongoClient = MongoClients.create(connString);
+        MongoDatabase database = mongoClient.getDatabase("sample_weatherdata");
+        MongoCollection<Document> coll = database.getCollection("data");
 
-
-        System.out.println("Plugin has started up");
+        this.getCommand("description").setExecutor(new CommandDescription());
+        this.saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
     }
 
     @Override
