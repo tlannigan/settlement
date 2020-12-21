b6a9ad117295341162b7074f77c82e3b340b4988
tlannigan <tristanlannigan@gmail.com>
2020-12-20 21:32:48 -0700

    Refactor pom.xml

--- src/main/java/ca/tlannigan/settlement/Settlement.java
+++ src/main/java/ca/tlannigan/settlement/Settlement.java
@@ -9,6 +9,15 @@ public class Settlement extends JavaPlugin {
         getLogger().info("Settlements will reign");
         this.getCommand("description").setExecutor(new CommandDescription());
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+
+        // Connect to MongoDB
+//        MongoClient mongoClient = MongoClients.create(
+//                "mongodb+srv://tlannigan:PmU9F334CtgZiuC@spigotcluster.ahu5k.mongodb.net/sample_weatherdata?retryWrites=true&w=majority");
+//        MongoDatabase database = mongoClient.getDatabase("sample_weatherdata");
+//        MongoCollection<Document> coll = database.getCollection("data");
+
+
+        System.out.println("Plugin has started up");
     }
 
     @Override
